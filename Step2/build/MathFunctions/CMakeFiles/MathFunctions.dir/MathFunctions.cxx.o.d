9b3aa8de8f9a802d7dbc0a71f23949224db8eb93
fywc <imfywc@gmail.com>
2023-07-28 01:14:11 +0800

    finish Step2

--- Step2/build/MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o.d
+++
@@ -1,5 +0,0 @@
-MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o: \
- /home/hanzh/code_src/c/cmake-3.27.0-tutorial-source/Step2/MathFunctions/MathFunctions.cxx \
- /usr/include/stdc-predef.h \
- /home/hanzh/code_src/c/cmake-3.27.0-tutorial-source/Step2/MathFunctions/MathFunctions.h \
- /home/hanzh/code_src/c/cmake-3.27.0-tutorial-source/Step2/MathFunctions/mysqrt.h
